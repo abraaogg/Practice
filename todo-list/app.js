adf9bed94c0924b8053f35c7c31c3734e2af42eb
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-08-13 15:26:01 -0300

    ToDo-list

---
+++ todo-list/app.js
@@ -0,0 +1,35 @@
+
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newTask input').value.length == 0){
+        alert('Please enter a task')
+    } 
+    else{
+        document.querySelector('#tasks').innerHTML += `
+
+        <div class = 'task'>
+        
+            <span id = 'taskname'>
+                ${document.querySelector('#newTask input').value}
+            </span>
+            <button class='delete'>
+            <i class="fa-solid fa-trash"></i>
+            </button>
+        </div>
+        `;
+
+        var currentTasks = document.querySelectorAll('.delete');
+        for(var i=0; i<currentTasks.length; i++){
+            currentTasks[i].onclick = function(){
+                this.parentNode.remove()
+            }
+        }
+
+        var tasks = document.querySelectorAll('.task');
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+        document.querySelector('#newTask input').value = ''
+    }
+}
