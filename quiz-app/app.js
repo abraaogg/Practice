963162221b96876de7dd60a81328b58ba4eb9356
abraaogg <abraaogoncalves1022@gmail.com>
2025-11-05 10:05:20 -0300

    Update app.js

---
+++ quiz-app/app.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    question: "Which is largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which is largest country in the world?",
+    answers: [
+      { text: "US", correct: false },
+      { text: "Canada", correct: false },
+      { text: "Russia", correct: true },
+      { text: "Brazil", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
