b1eb74f397f31abf898b88881b4d7819a76d615a
abraaogg <abraaogoncalves1022@gmail.com>
2025-12-10 13:12:28 -0300

    html

---
+++ 30Js-projects/validation-form/app.js
@@ -0,0 +1,13 @@
+let icon = document.getElementById("icon");
+
+icon.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  } else {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
+});
