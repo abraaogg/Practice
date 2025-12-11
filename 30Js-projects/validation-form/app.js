a5b961c77e9de6a9d3f7d5476e0212c01924523a
abraaogg <abraaogoncalves1022@gmail.com>
2025-12-11 15:30:59 -0300

    validation-form

--- 30Js-projects/validation-form/app.js
+++ 30Js-projects/validation-form/app.js
@@ -1,13 +1,43 @@
-let icon = document.getElementById("icon");
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const phoneInput = document.getElementById("phone");
+const emailInput = document.getElementById("email");
 
-icon.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
+form.addEventListener("submit", (e) => {
+  let errors = getLoginFormErrors();
 
-  if (document.body.classList.contains("dark-theme")) {
-    icon.classList.remove("fa-moon");
-    icon.classList.add("fa-sun");
-  } else {
-    icon.classList.remove("fa-sun");
-    icon.classList.add("fa-moon");
+  if (errors.length > 0) {
+    e.preventDefault();
+    console.log(errors);
   }
 });
+
+function getLoginFormErrors() {
+  let errors = [];
+
+  // NAME
+  if (nameInput.value.trim() === "") {
+    errors.push("Name is required");
+    nameInput.parentElement.classList.add("incorrect");
+  } else {
+    nameInput.parentElement.classList.remove("incorrect");
+  }
+
+  // PHONE
+  if (phoneInput.value.trim() === "") {
+    errors.push("Phone is required");
+    phoneInput.parentElement.classList.add("incorrect");
+  } else {
+    phoneInput.parentElement.classList.remove("incorrect");
+  }
+
+  // EMAIL
+  if (emailInput.value.trim() === "") {
+    errors.push("Email is required");
+    emailInput.parentElement.classList.add("incorrect");
+  } else {
+    emailInput.parentElement.classList.remove("incorrect");
+  }
+
+  return errors;
+}
